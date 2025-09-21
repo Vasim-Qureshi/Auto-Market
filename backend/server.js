8272223bbf93f0afab082a7c4d3c285a50da578e
vasim-qureshi <vasimqureshi2000@gmail.com>
2025-09-21 16:21:48 +0530

    adding frontend url in backend

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 // Middleware
 app.use(cors(
   {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://auto-market-neon.vercel.app"],
     credentials: true, // Important for cookies
   }
 ));
