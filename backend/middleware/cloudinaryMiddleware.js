99df66f6d28193c382d20fca970c380ac513cc90
vasim-qureshi <vasimqureshi2000@gmail.com>
2025-09-29 19:32:52 +0530

    add Cloudinary url

--- backend/middleware/cloudinaryMiddleware.js
+++ backend/middleware/cloudinaryMiddleware.js
@@ -11,6 +11,7 @@ cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  url: process.env.CLOUDINARY_URL,
 });
 
 const storage = new CloudinaryStorage({
